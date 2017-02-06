7eb28cba609cbf9bed9748dc0d77ce776627adf5
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-06 09:48:58 -0800

    Django Template Include with Variable

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_save
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models.signals import post_save
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 # Create your models here.
 
@@ -65,6 +66,13 @@ class Variation(models.Model):
 
 	def get_absolute_url(self):
 		return self.product.get_absolute_url()
+
+	def get_html_price(self):
+		if self.sale_price is not None:
+			html_text = "<span class='sale-price'>{}</span><span class='original-price'>{}</span>".format(self.sale_price, self.price)
+		else:
+			html_text = "<span class='price'>{}</span>".format(self.price)
+		return mark_safe(html_text)
 
 def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
 	print(sender, instance, created)
