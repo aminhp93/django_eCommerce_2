fd2489aec78f0621aa2758f27f8ff5041e8e43ae
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-08 09:33:47 -0800

    Add, Remove, Update Cart

--- src/carts/models.py
+++ src/carts/models.py
@@ -5,6 +5,7 @@ from products.models import Variation
 
 # Create your models here.
 class CartItem(models.Model):
+	cart = models.ForeignKey("Cart")
 	item = models.ForeignKey(Variation)
 	quantity = models.PositiveIntegerField(default=1)
 
@@ -13,7 +14,7 @@ class CartItem(models.Model):
 
 class Cart(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
-	items = models.ManyToManyField(Variation)
+	items = models.ManyToManyField(Variation, through=CartItem)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
