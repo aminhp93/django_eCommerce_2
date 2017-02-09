4fee1197f1c5378bdf1b288909a42a7eb6fee1d9
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-08 19:13:18 -0800

    Format cart and Remove items

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import View
+from django.views.generic.detail import SingleObjectMixin
+
+from products.models import Variation
+from carts.models import Cart, CartItem
 
 # Create your views here.
+
+class CartView(SingleObjectMixin, View):
+	model = Cart 
+	template_name = "carts/view.html"
+
+	def get_object(self, *args, **kwargs):
+		# request.session.set_expiry(300) # 5 minutes
+		cart_id = self.request.session.get("cart_id")
+		if cart_id == None:
+			cart = Cart()
+			cart.save()
+			cart_id = cart.id
+			self.request.session["cart_id"] = cart_id
+		cart = Cart.objects.get(id=cart_id)
+		if self.request.user.is_authenticated():
+			cart.user = self.request.user
+			cart.save()
+		return cart
+
+	def get(self, request, *args, **kwargs):
+		cart = self.get_object()
+		item_id = request.GET.get("item")
+		delete_item = request.GET.get("delete")
+		if item_id:
+			item_instance = get_object_or_404(Variation, id=item_id)
+			quantity = request.GET.get("quantity", 1)
+			try:
+				if quantity < 1:
+					delete_item = True
+			except:
+				raise Http404
+
+			cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
+			if delete_item:
+				cart_item.delete()
+			else:
+				cart_item.quantity = quantity
+				cart_item.save()
+
+		context = {
+			"object": self.get_object()
+		}
+		template = self.template_name
+		return render(request, template, context)
--- src/e_commerce_2/urls.py
+++ src/e_commerce_2/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 from newsletter import views as newsletter_views
 from e_commerce_2 import views as e_commerce_2_views
+from carts.views import CartView
 
 urlpatterns = [
     url(r'^$', newsletter_views.home, name='home'),
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^products/', include('products.urls')),
     url(r'^categories/', include('products.urls_categories')),
+    url(r'^carts/', CartView.as_view(), name='carts'),
 ]
 
 if settings.DEBUG:
